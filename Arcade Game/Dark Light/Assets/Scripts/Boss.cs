e773c3d066c971588348fd2f7861b822f053ffa8
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-04-10 09:07:14 +1000

    First

---
+++ Arcade Game/Dark Light/Assets/Scripts/Boss.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Boss
+{
+	public class Boss : MonoBehaviour 
+	{
+		//Integers:
+		private int tTimer = 0; //Tick rate timer
+		public int sTimer = 0; //Start timer
+		public int aTimer = 0; //Attack timer
+		//private int baTimer = 0; //Basic Attack timer
+		//private int jTimer = 0; //Jump timer
+		//private int cTimer = 0; //Charge timer
+		//private int bTimer = 0; //Burst timer
+        private int rTimer = 60; //Rest timer
+		private int basicV = 0; //Basic Attack's value
+		private int jumpV = 0; //Jump Attack's value
+		private int chargeV = 0; //Charge Attack's value
+		private int burstV = 0; //Burst Attack's value
+		private int phase = 0; //Determines the difficulty of the boss
+        private int move = 0; //Counts up everytime an ability is used
+
+        //Floats:
+        public float distanceFP = 0f; //Tracks how far away the Player is from the boss
+        public int aRange = 0; //Used to randomly select an attack pattern
+	 
+		//Booleans:
+		private bool startPeriod = true; //Checks whether or not it's the start of an instance
+		private bool canAttack = false; //Checks whether or not the boss is allowed to attack
+		private bool jumpAttack = false; //Checks whether or not a jump attack has been selected
+		private bool chargeAttack = false; //Checks whether or not a charge attack has been selected
+		private bool burstAttack = false; //Checks whether or not a burst attack has been selected
+		private bool secondPhase = false; //Checks whether or not the second phase has activated
+        private bool attackPossible = true; //Checks whether or no an attack is avaliable for use
+        private bool attackReset = true; //Checks whether or not the attack pattern needs to be reset
+        private bool waiting = false; //Checks whether or not an attack has finished and starts the rest period
+        private bool attackActive = false; //Checks whether or not a timer is needed to finish an attack
+
+        //Reference:
+        public Transform Player;
+
+		void Start ()
+		{
+		
+		}
+
+		void FixedUpdate()  
+		{
+            distanceFP = Vector2.Distance(transform.position, Player.position);
+
+			if (startPeriod == true)
+			{
+				sTimer++;
+				if (sTimer == 240)
+				{
+					startPeriod = false;
+					sTimer = 0;
+				}
+			}
+			else if (startPeriod == false)
+			{
+			    CalcAttack();
+			}
+		}
+
+		public void CalcAttack()
+		{ 
+			if (attackPossible == true && attackReset == false) //aTimer >= 0 &&
+
+            {
+				//aTimer--;
+                if (distanceFP <= 10)
+                {
+                    if (aRange == 1)
+                    {
+                        Basic();
+                        print("Basic");
+                    }
+                    else if (aRange == 2)
+                    {
+                        Charge();
+                        print("Charge");
+                    }
+                    else if (aRange == 3)
+                    {
+                        if (secondPhase == true)
+                        {
+                            Burst();
+                            print("Burst");
+                        }
+                        else
+                        {
+                            attackReset = true;
+                            print("Redo");
+                        }
+                    }
+                }
+                else
+                {
+                    Jump();
+                    print("Jump");
+                }
+			}
+            if (attackReset == true)
+            {
+                aRange = Random.Range(0, 101); //only 3 types of attacks
+                if (aRange <= 100 && aRange >= 51)
+                {
+                    aRange = 1;
+                }
+                else if (aRange <= 50  && aRange >= 11)
+                {
+                    aRange = 2;
+                }
+                else if (aRange <= 1 && aRange >= 10)
+                {
+                    aRange = 3;
+                }
+                attackReset = false;
+            }
+            //if (attackActive == true)
+            //{
+            //    if (aTimer >= 0)
+            //    {
+            //        aTimer--;
+            //    }
+            //}
+            if (waiting == true)
+            {
+                Waiting();
+            }
+
+		}
+
+		public void Basic()
+		{
+            aTimer = basicV;
+			if (aTimer >= 0)
+			{
+				aTimer--;
+			}
+
+		}
+
+		public void Charge()
+		{
+            aTimer = chargeV;
+			if (aTimer >= 0)
+			{
+				aTimer--;
+			}
+        }	
+
+		public void Jump()
+		{
+            aTimer = jumpV;
+			if (aTimer >= 0)
+			{
+				aTimer--;
+			}
+        }
+
+		public void Burst()
+		{
+            aTimer = burstV;
+			if (aTimer >= 0)
+			{
+				aTimer--;
+			}
+        }
+
+        public void Waiting()
+        {
+            rTimer--;
+            if (rTimer == 0)
+            {
+                rTimer = 60;
+            }
+        }
+	}
+}
---
+++ Arcade Game/Dark Light/Assets/Scripts/Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[AddComponentMenu("Player Script")]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    //Mechanics 
+    private int accel = 3; //default value for dash.
+    private int health = 6; //default value got player health.
+    private int damage = 1; //temp, may be moved to child class (sword/weapon).
+    private float xSpeed; //default value for movement on the x-axis.
+    private float ySpeed; //default value for movement on the y-axis.
+    private float gravity; //default value of gravity for player.
+    private bool iFrame = false; //tested for whether or not Dash has been given an iFrame.
+    private bool dash = false; //activates and locks when dash hotkey is pressed.
+    private bool attack = false; //activates and locks when attack hotkey is pressed.
+    private bool beenHit = false; //activates and locks to give an additional iFrame for a brief moment after the player has been hit.
+    private bool facing = true; //true = right, false = left. Changes depending on what face the player is changing.
+    //Counters
+    private int iFCounter = 0; //counter for iFrame activation.
+    private int aCounter = 0; //counter for attack activation.
+    private int dCounter = 0; //counter for dash activation.
+
+    public void Start() 
+	{
+	}
+    public void FixedUpdate() //basic update cycle.
+    {
+        Movement();
+        Attack();
+        Dash();
+        IFrame();
+        Health();
+    }
+    //[Header("Movement")]
+    public void Movement() //deals with input for movement.
+    {
+        if (Input.GetKeyDown("w"))
+        {
+
+        }
+        if (Input.GetKeyDown("a"))
+        {
+
+        }
+        if (Input.GetKeyDown("s"))
+        {
+
+        }
+        if (Input.GetKeyDown("d"))
+        {
+
+        }
+    }
+    //[Header("Attack")]
+    public void Attack() //deals with attack activation sequence.
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            //boss.Heath = Boss.Health - Damage; (boss doesn't exist currently)
+        }
+    }
+    //[Header("Dash")]
+    public void Dash() //deals with the activation of dash.
+    {
+        if (Input.GetKeyDown("left shift"))
+        {
+            if (facing == true)
+            {
+                iFrame = true;
+                //vector2, required for movement, learn this. 
+            }
+            else
+            {
+                iFrame = true;
+                //vector2, required for movement, learn this. 
+            }
+        }
+    }
+    public void IFrame() //deals with the activation of the iFrame after certain activations.
+    {
+        if (iFrame == true)
+        {
+            iFCounter++;
+            if (iFCounter <= 30*Time.deltaTime)
+            {
+                iFCounter = 0;
+                iFrame = false;
+            }
+        }
+    }
+    //[Header("Health")]
+    public void Health() //deals with health deduction and external UI changes.
+    {
+        if (beenHit == true && iFrame == false && health >= 1)
+        {
+            health--;
+            iFrame = true;
+            beenHit = false;
+        }
+        if (health == 0)
+        {
+            //UI.Gameover = true;
+        }
+    }
+}
