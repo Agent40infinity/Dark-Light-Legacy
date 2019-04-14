14bc336abdc82e1088e79604a6c6fdbe49b3eccd
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-04-14 14:52:07 +1000

    v0.0.2 - Attacking Implementation

--- Arcade Game/Dark Light/Assets/Scripts/Entities/Player.cs
+++ Arcade Game/Dark Light/Assets/Scripts/Entities/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [AddComponentMenu("Player Script")]
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
@@ -10,16 +11,16 @@ public class Player : MonoBehaviour
     private int accel = 3; //default value for dash.
     private int health = 6; //default value got player health.
     private int damage = 1; //temp, may be moved to child class (sword/weapon).
-    private float xSpeed = 40f; //default value for movement on the x-axis.
-    private float ySpeed; //default value for movement on the y-axis.
-    private float gravity; //default value of gravity for player.
-    private float force; //default value of the force applied to the player.
-    private bool hasJumped = false; //default value for whether the player is on the ground or not.
     private bool iFrame = false; //tested for whether or not Dash has been given an iFrame.
     private bool dash = false; //activates and locks when dash hotkey is pressed.
     private bool attack = false; //activates and locks when attack hotkey is pressed.
     private bool beenHit = false; //activates and locks to give an additional iFrame for a brief moment after the player has been hit.
     private bool facing = true; //true = right, false = left. Changes depending on what face the player is changing.
+
+    //Attacking:
+    private float attackCooldown;
+    private float startACooldown;
+    public float attackRange;
 
     //Counters:
     private int iFCounter = 0; //counter for iFrame activation.
@@ -27,6 +28,8 @@ public class Player : MonoBehaviour
     private int dCounter = 0; //counter for dash activation.
 
     //Reference:
+    public Transform attackPos;
+    public LayerMask isEnemy;
 
     public void Start() 
 	{
@@ -38,12 +41,36 @@ public class Player : MonoBehaviour
         IFrame();
         Health();
     }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
     //[Header("Attack")]
     public void Attack() //deals with attack activation sequence.
     {
+        if (attackCooldown <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(attackPos.position, attackRange, isEnemy);
+                for (int i = 0; i < enemiesInRange.Length; i++)
+                {
+                    //enemiesInRange[i].GetComponent<Enemy>.TakeDamage(damage);     
+                    //boss.Heath = Boss.Health - Damage; (boss doesn't exist currently)
+                }
+            }
+            attackCooldown = startACooldown;
+        }
+        else
+        {
+            attackCooldown -= Time.deltaTime;
+        }
+
         if (Input.GetKeyDown("e"))
         {
-            //boss.Heath = Boss.Health - Damage; (boss doesn't exist currently)
+            
         }
     }
     //[Header("Dash")]
--- Arcade Game/Dark Light/Assets/Scripts/Utility/PlayerController.cs
+++ Arcade Game/Dark Light/Assets/Scripts/Utility/PlayerController.cs
@@ -2,71 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Physics
 {
-    //Core:
-    public float xSpeed = 12f; //default value for movement on the x-axis.
-    public float ySpeed = 12f; //default value for movement on the y-axis.
-    private float gravity; //default value of gravity for player.
-    private float force; //default value of the force applied to the player.
-    private bool isJumping; //Default value of whether the player is jumping.
-    private bool isGrounded; //default value for whether the player is on the ground or not.
-    public float checkRadius; //Creates a radius to check for the ground.
-
-    //Timers/Counters:
-    private float aTTimer; //Air time timer.
-    public float airTime = 0.1f; //Air time counter.
-
-    //Reference:
-    private Rigidbody2D rigid; //References the RigidBody2D for player.
-    public Transform feetPos; //Used to reference the ground check for player.
-    public LayerMask isWalkable; //Used to create reference to walkable objects.
-
-	public void Start() 
+    public class PlayerController : MonoBehaviour
     {
-        rigid = GetComponent<Rigidbody2D>();
-	}
+        #region Variables
+        //Core:
+        public float xSpeed = 12f; //default value for movement on the x-axis.
+        public float ySpeed = 12f; //default value for movement on the y-axis.
+        private float gravity; //default value of gravity for player.
+        private float force; //default value of the force applied to the player.
+        private bool isJumping; //Default value of whether the player is jumping.
+        private bool isGrounded; //default value for whether the player is on the ground or not.
+        public float checkRadius; //Creates a radius to check for the ground.
 
-    public void Update() 
-    {
-        Movement();
-    }
+        //Timers/Counters:
+        private float aTTimer; //Air time timer.
+        public float airTime = 0.1f; //Air time counter.
 
-    public void Movement() //Normal Movement - used for vertical input and movement.
-    {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, isWalkable); //Checks for if the player is grounded or not.
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)) //Checks if the player is grounded and space has been pressed - light jump.
+        //Reference:
+        private Rigidbody2D rigid; //References the RigidBody2D for player.
+        public Transform feetPos; //Used to reference the ground check for player.
+        public LayerMask isWalkable; //Used to create reference to walkable objects.
+        #endregion
+
+        #region General
+        public void Start()
         {
-            isJumping = true;
-            aTTimer = airTime;
-            rigid.velocity = Vector2.up * ySpeed;
+            rigid = GetComponent<Rigidbody2D>();
         }
-        if (Input.GetKey(KeyCode.Space) && isJumping == true) //Checks if space has been pressed and that the player is in the air.
+
+        public void Update()
         {
-            if (aTTimer > 0) //Checks the timer to allow the player to jump higher.
+            Movement();
+        }
+
+        public void FixedUpdate()
+        {
+            MovementF();
+        }
+        #endregion
+
+        #region Movement - Update
+        public void Movement() //Normal Movement - used for vertical input and movement.
+        {
+            isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, isWalkable); //Checks for if the player is grounded or not.
+            if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)) //Checks if the player is grounded and space has been pressed - light jump.
             {
+                isJumping = true;
+                aTTimer = airTime;
                 rigid.velocity = Vector2.up * ySpeed;
-                aTTimer -= Time.deltaTime;
             }
-            else 
+            if (Input.GetKey(KeyCode.Space) && isJumping == true) //Checks if space has been pressed and that the player is in the air.
+            {
+                if (aTTimer > 0) //Checks the timer to allow the player to jump higher.
+                {
+                    rigid.velocity = Vector2.up * ySpeed;
+                    aTTimer -= Time.deltaTime;
+                }
+                else
+                {
+                    isJumping = false;
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.Space)) //Makes sure jumping isn't active when space isn't pressed.
             {
                 isJumping = false;
             }
         }
-        if (Input.GetKeyUp(KeyCode.Space)) //Makes sure jumping isn't active when space isn't pressed.
+        #endregion
+
+        #region Movement - Fixed
+        public void MovementF() //Fixed Movement - Allows for horizontal input and movement.
         {
-            isJumping = false;
+            force = Input.GetAxisRaw("Horizontal");
+            rigid.velocity = new Vector2(force * xSpeed, rigid.velocity.y);
         }
+        #endregion
     }
-
-    public void FixedUpdate()
-    {
-        MovementF();
-    }
-
-	public void MovementF() //Fixed Movement - Allows for horizontal input and movement.
-    {
-        force = Input.GetAxisRaw("Horizontal");
-        rigid.velocity = new Vector2(force * xSpeed, rigid.velocity.y);
-	}
 }
