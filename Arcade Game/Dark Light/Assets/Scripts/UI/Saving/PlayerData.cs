1197c039ea00348d8bed08be4c9974efa420ba24
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-05-27 10:50:04 +1000

    xx

---
+++ Arcade Game/Dark Light/Assets/Scripts/UI/Saving/PlayerData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int level;
+    public int health;
+    public float[] position = new float[3];
+
+    public PlayerData(Player player) //Creates a reference for the Player and is used as the baseline for all data being saved into "save.dat".
+    {
+        //level = player.level;
+        health = Player.curHealth;
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+
+    }
+}
