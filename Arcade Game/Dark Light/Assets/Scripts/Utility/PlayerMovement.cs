8fc5da854b8790a02b3fe9c3e556fa48316a7bcb
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-05-29 10:00:16 +1000

    x

--- Arcade Game/Dark Light/Assets/Scripts/Utility/PlayerMovement.cs
+++ Arcade Game/Dark Light/Assets/Scripts/Utility/PlayerMovement.cs
@@ -2,158 +2,157 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class PlayerMovement : MonoBehaviour
-    {
+public class PlayerMovement : MonoBehaviour
+{
     #region Variables
     //Core:
-    //public YoloMotherFucker locks;
-        public float xSpeed = 12f; //Default value for movement on the x-axis.
-        public float ySpeed = 14f; //Default value for movement on the y-axis.
-        public float yLimiter = 0.5f; //Default value for the limiter placed on the y-axis.
-        private float gravity; //Default value of gravity for player.
-        private float force; //Default value of the force applied to the player.
-        private bool isJumping; //Default value of whether the player is jumping.
-        public bool isFacing; //What direction is the player facing? true = right, false = left.
-        public bool isGrounded; //Default value for whether the player is on the ground or not.
-        public float checkRadius; //Creates a radius to check for the ground.
-        public float accelSpeed = 4f; //Default value for dash's speed.
-        public bool dash = false; //Used to call upon the sub-routine (dash).
-		public bool canDash = true; //Used to check whether or not the player is able to dash.
-		public bool dashReset = false; //Used to see whether or not the dash can be reset.
-		public bool dashCooldown = false; //Used to see whether or not the dash is on cooldown.
-        public bool lockMovement = false; //Used to check whether or not all movment is required to be locked.
-        public bool lockYAxis = false; //Used to lock the Y Axis.
-        public bool unlockYAxis = false; //Used to Unlock the Y Axis.
-        public Vector2 tempGravity; //Used to temporarily store the value of gravity.
-        public Vector2 tempYVelocity; //Used to temporarily store the value of the Y velocity.
+    public float xSpeed = 12f; //Default value for movement on the x-axis.
+    public float ySpeed = 14f; //Default value for movement on the y-axis.
+    public float yLimiter = 0.5f; //Default value for the limiter placed on the y-axis.
+    private float gravity; //Default value of gravity for player.
+    private float force; //Default value of the force applied to the player.
+    private bool isJumping; //Default value of whether the player is jumping.
+    public bool isFacing; //What direction is the player facing? true = right, false = left.
+    public bool isGrounded; //Default value for whether the player is on the ground or not.
+    public float checkRadius; //Creates a radius to check for the ground.
+    public float accelSpeed = 4f; //Default value for dash's speed.
+    public bool dash = false; //Used to call upon the sub-routine (dash).
+    public bool canDash = true; //Used to check whether or not the player is able to dash.
+    public bool dashReset = false; //Used to see whether or not the dash can be reset.
+    public bool dashCooldown = false; //Used to see whether or not the dash is on cooldown.
+    public bool lockMovement = false; //Used to check whether or not all movment is required to be locked.
+    public bool lockYAxis = false; //Used to lock the Y Axis.
+    public bool unlockYAxis = false; //Used to Unlock the Y Axis.
+    public Vector2 tempGravity; //Used to temporarily store the value of gravity.
+    public Vector2 tempYVelocity; //Used to temporarily store the value of the Y velocity.
 
-        //Timers/Counters:
-        private float aTTimer; //Air time timer.
-        public float airTime = 0.1f; //Air time counter.
-        public int aHTimer;
-        public int airHTime = 5;
-        public float dashTimer; //Dash time timer.
-        public float dashTimeReset = 0.15f; //Dash time reset.
-		public float dashCTime = 0.5f; //Dash cooldown reset.
-		public float dCTimer = 0.5f; //Dash cooldown timer
+    //Timers/Counters:
+    private float aTTimer; //Air time timer.
+    public float airTime = 0.1f; //Air time counter.
+    public int aHTimer;
+    public int airHTime = 5;
+    public float dashTimer; //Dash time timer.
+    public float dashTimeReset = 0.15f; //Dash time reset.
+    public float dashCTime = 0.5f; //Dash cooldown reset.
+    public float dCTimer = 0.5f; //Dash cooldown timer
 
-        //Reference:
-        private Rigidbody2D rigid; //References the RigidBody2D for player.
-        public Transform feetPos; //Used to reference the ground check for player.
-        public LayerMask isWalkable; //Used to create reference to walkable objects.
-        #endregion
+    //Reference:
+    private Rigidbody2D rigid; //References the RigidBody2D for player.
+    public Transform feetPos; //Used to reference the ground check for player.
+    public LayerMask isWalkable; //Used to create reference to walkable objects.
+    #endregion
 
-        #region General
-        public void Start()
-        {
-            rigid = GetComponent<Rigidbody2D>();
-            dashTimer = dashTimeReset;
-            isFacing = true; //Defaults the player to look right.
-            tempGravity = Physics2D.gravity;
-        }
+    #region General
+    public void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        dashTimer = dashTimeReset;
+        isFacing = true; //Defaults the player to look right.
+        tempGravity = Physics2D.gravity;
+    }
 
-        public void Update()
-        {
+    public void Update()
+    {
 
-        
-            Debug.Log("Gravity before unlock: " + Physics2D.gravity);
-            //Debug.Log("Facing Right? " + isFacing);
-            //Debug.Log((int)Input.GetAxis("Horizontal"));
-            if (lockMovement == false)
-            {
-                Movement();
-            }
 
-            if (dash == true)
-            {
-                Dash();
-            }
-        }
-
-        public void FixedUpdate()
-        {
+        Debug.Log("Gravity before unlock: " + Physics2D.gravity);
+        //Debug.Log("Facing Right? " + isFacing);
+        //Debug.Log((int)Input.GetAxis("Horizontal"));
         if (lockMovement == false)
+        {
+            Movement();
+        }
+
+        if (dash == true)
+        {
+            Dash();
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (lockMovement == false)
+        {
+            MovementF();
+        }
+    }
+    #endregion
+
+    #region Movement - Update
+    public void Movement() //Normal Movement - used for vertical input and movement.
+    {
+        if ((int)Input.GetAxisRaw("Horizontal") == -1) //Determines whether or not the player is Facing left.
+        {
+            isFacing = false;
+
+        }
+        else if ((int)Input.GetAxisRaw("Horizontal") == 1) //Determines whether or not the player is Facing right.
+        {
+            isFacing = true;
+        }
+        if (dashCooldown == true)  //Puts dash on cooldown.
+        {
+            if (dCTimer >= 0)
             {
-                MovementF();
+                Debug.Log(dCTimer);
+                dCTimer -= Time.deltaTime;
+            }
+            else
+            {
+                dCTimer = dashCTime;
+                dashReset = true;
+                dashCooldown = false;
             }
         }
-        #endregion
-
-        #region Movement - Update
-        public void Movement() //Normal Movement - used for vertical input and movement.
+        if (isGrounded == true && dashReset == true) //Resets the Dash 
         {
-            if ((int)Input.GetAxisRaw("Horizontal") == -1) //Determines whether or not the player is Facing left.
+            canDash = true;
+            dashReset = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift)) //Checks whether or not the player is attempting to dash.
+        {
+            if (canDash == true)
             {
-                isFacing = false;
-
-            }
-            else if ((int)Input.GetAxisRaw("Horizontal") == 1) //Determines whether or not the player is Facing right.
-            {
-                isFacing = true;
-            }
-			if (dashCooldown == true)  //Puts dash on cooldown.
-			{
-				if (dCTimer >= 0)
-				{
-                    Debug.Log(dCTimer);
-					dCTimer -= Time.deltaTime;
-				}
-				else 
-				{
-					dCTimer = dashCTime;
-					dashReset = true;
-					dashCooldown = false;
-				}
-			}
-			if (isGrounded == true && dashReset == true) //Resets the Dash 
-			{
-				canDash = true;
-                dashReset = false;
-			}
-            if (Input.GetKeyDown(KeyCode.LeftShift)) //Checks whether or not the player is attempting to dash.
-            {
-				if (canDash == true)
-				{
-					dash = true;
-					lockMovement = true;
-				}
-            }
-
-            isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, isWalkable); //Checks for if the player is grounded or not.
-            if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)) //Checks if the player is grounded and space has been pressed - light jump.
-            {
-                rigid.velocity = Vector2.up * ySpeed * yLimiter;
-                aTTimer = airTime;
-                isJumping = true;
-            }
-            if (Input.GetKey(KeyCode.Space) && isJumping == true) //Checks if space has been pressed and that the player is in the air.
-            {
-                if (aTTimer > 0) //Checks the timer to allow the player to jump higher.
-                {
-                    rigid.velocity = Vector2.up * ySpeed;
-                    aTTimer -= Time.deltaTime;
-                }
-                else
-                {
-                    isJumping = false;
-                }
-            }
-            if (Input.GetKeyUp(KeyCode.Space)) //Makes sure jumping isn't active when space isn't pressed.
-            {
-                Vector2 jX = rigid.velocity;
-                if (rigid.velocity.y >= yLimiter)
-                {
-                    rigid.velocity = new Vector2(jX.x, 0);
-                    isJumping = false;
-                }
-
+                dash = true;
+                lockMovement = true;
             }
         }
-        #endregion
 
-        #region Dash - Update
-        public void Dash() //Movement: Dash - Allows the player to dash forward.
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, isWalkable); //Checks for if the player is grounded or not.
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)) //Checks if the player is grounded and space has been pressed - light jump.
         {
+            rigid.velocity = Vector2.up * ySpeed * yLimiter;
+            aTTimer = airTime;
+            isJumping = true;
+        }
+        if (Input.GetKey(KeyCode.Space) && isJumping == true) //Checks if space has been pressed and that the player is in the air.
+        {
+            if (aTTimer > 0) //Checks the timer to allow the player to jump higher.
+            {
+                rigid.velocity = Vector2.up * ySpeed;
+                aTTimer -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) //Makes sure jumping isn't active when space isn't pressed.
+        {
+            Vector2 jX = rigid.velocity;
+            if (rigid.velocity.y >= yLimiter)
+            {
+                rigid.velocity = new Vector2(jX.x, 0);
+                isJumping = false;
+            }
+
+        }
+    }
+    #endregion
+
+    #region Dash - Update
+    public void Dash() //Movement: Dash - Allows the player to dash forward.
+    {
         if (dash == true) //Double check that dash is activated.S
         {
             if (dashTimer >= 0) //Checks if the dash timer is being counted.
@@ -200,16 +199,16 @@ using UnityEngine;
             unlockYAxis = false;
         }
     }
-        #endregion
+    #endregion
 
-        #region Movement - Fixed
-        public void MovementF() //Fixed Movement - Allows for horizontal input and movement.
-        {
-            force = Input.GetAxisRaw("Horizontal"); 
-            rigid.velocity = new Vector2(force * xSpeed, rigid.velocity.y);
-        }
-        #endregion
+    #region Movement - Fixed
+    public void MovementF() //Fixed Movement - Allows for horizontal input and movement.
+    {
+        force = Input.GetAxisRaw("Horizontal");
+        rigid.velocity = new Vector2(force * xSpeed, rigid.velocity.y);
     }
+    #endregion
+}
 
 
 
