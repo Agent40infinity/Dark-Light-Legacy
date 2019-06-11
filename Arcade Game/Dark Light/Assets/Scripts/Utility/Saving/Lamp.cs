6eb33244f865b1abf4aba97514168dbd089ffcfe
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-06-11 13:29:55 +1000

    x

--- Arcade Game/Dark Light/Assets/Scripts/Utility/Saving/Lamp.cs
+++ Arcade Game/Dark Light/Assets/Scripts/Utility/Saving/Lamp.cs
@@ -9,14 +9,12 @@ using UnityEngine;
 public class Lamp : MonoBehaviour
 {
     public GameObject lamps;
-    public Vector3 lParent;
-    public Vector3[] lPos = new Vector3[3];
+    public Transform[] lPos;
 
     public void Start()
     {
         lamps = GameObject.Find("Lamps");
-        lParent = lamps.transform.position;
-        //lPos = lamps.GetComponentInChildren<Transform>().position;
+        lPos = lamps.GetComponentsInChildren<Transform>();
     }
 
 }
