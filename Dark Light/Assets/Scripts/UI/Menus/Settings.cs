b87fb1997547e7d06e38f55d845f6d717f69b4a4
Aiden Nathan <agent9@y7mail.com>
2021-06-18 18:39:13 +1000

    Pre Setting - Menu Split

--- Dark Light/Assets/Scripts/UI/Menus/Settings.cs
+++ Dark Light/Assets/Scripts/UI/Menus/Settings.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Settings : MonoBehaviour
 {
-    
+    public void Start()
+    {
+        up.text = GameManager.keybind["Up"].ToString();
+        down.text = GameManager.keybind["Down"].ToString();
+        left.text = GameManager.keybind["Left"].ToString();
+        right.text = GameManager.keybind["Right"].ToString();
+        jump.text = GameManager.keybind["Jump"].ToString();
+        attack.text = GameManager.keybind["Attack"].ToString();
+        dash.text = GameManager.keybind["Dash"].ToString();
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 }
