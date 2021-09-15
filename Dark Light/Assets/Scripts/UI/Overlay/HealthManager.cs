c980504eed5391cac2dc7b283b7187bffb9961cd
Aiden Nathan <agent9@y7mail.com>
2021-09-15 22:59:48 +1000

    x

--- Dark Light/Assets/Scripts/UI/Overlay/HealthManager.cs
+++ Dark Light/Assets/Scripts/UI/Overlay/HealthManager.cs
@@ -12,8 +12,6 @@ namespace HealthManagement
     public class HealthManager : MonoBehaviour
     {
         #region Variables
-        public Image[] heartSlots = new Image[5]; //Array used to reference the individual heart locations.
-        public Sprite[] hearts = new Sprite[2]; //Array used to reference the images used per heart.
         private float imagesPerHeart; //Defines how many Images there are per heart slot.
         public bool checkHealth = true;
 
@@ -24,7 +22,6 @@ namespace HealthManagement
         public void Start()
         {
             anim = GetComponentsInChildren<Animator>();
-            heartSlots = GetComponentsInChildren<Image>();
 
             if (checkHealth == true)
             {
@@ -34,7 +31,7 @@ namespace HealthManagement
 
         public void Update() //Changes the image depending on how much health the player has.
         {
-            for (int i = 0; i < heartSlots.Length; i++)
+            for (int i = 0; i < anim.Length; i++)
             {
                 if (Player.curHealth >= (imagesPerHeart * 2) * (i + 1)) 
                 {
@@ -60,7 +57,7 @@ namespace HealthManagement
         #region CheckForHealth
         public void CheckForHealth() //Calculates the health per slot.
         {
-            imagesPerHeart = (float)Player.maxHealth / (float)(heartSlots.Length * 2.5f); //0.5
+            imagesPerHeart = (float)Player.maxHealth / (float)(anim.Length * 2.5f); //0.5
             checkHealth = false;
 
         }
